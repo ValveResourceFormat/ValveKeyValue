29e113364150197ffb38ced948923710ebe2a1b6
Yaakov <pubgit@codingrange.com>
2016-02-27 23:46:58 +1100

    Create initial projects

---
+++ ValveKeyValue/ValveKeyValue/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Valve KeyValue Library")]
+[assembly: AssemblyDescription("Library to parse and write Valve KeyValue formats")]
+[assembly: AssemblyCompany("SteamDatabase")]
+[assembly: AssemblyProduct("ValveKeyValue")]
+[assembly: AssemblyCopyright("Copyright © SteamDatabase 2016")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("df62ad4a-c7f4-43ab-b198-3f4009794437")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
