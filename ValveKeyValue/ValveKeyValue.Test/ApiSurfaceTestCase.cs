6dd9da5102eba52dcd9ac42567761dd7308db755
Yaakov <pubgit@codingrange.com>
2016-04-12 22:19:53 +1000

    API surface tests: missed a few type aliases, sort alphabetically.

--- ValveKeyValue/ValveKeyValue.Test/ApiSurfaceTestCase.cs
+++ ValveKeyValue/ValveKeyValue.Test/ApiSurfaceTestCase.cs
@@ -155,53 +155,69 @@ namespace ValveKeyValue.Test
 
         static string GetTypeAsString(Type type)
         {
-            if (type == typeof(void))
-            {
-                return "void";
-            }
-            else if (type == typeof(bool))
+            if (type == typeof(bool))
             {
                 return "bool";
-            }
-            else if (type == typeof(string))
-            {
-                return "string";
-            }
-            else if (type == typeof(int))
-            {
-                return "int";
-            }
-            else if (type == typeof(uint))
-            {
-                return "uint";
-            }
-            else if (type == typeof(short))
-            {
-                return "short";
-            }
-            else if (type == typeof(ushort))
-            {
-                return "ushort";
             }
             else if (type == typeof(byte))
             {
                 return "byte";
             }
-            else if (type == typeof(sbyte))
+            else if (type == typeof(char))
             {
-                return "sbyte";
+                return "char";
+            }
+            else if (type == typeof(decimal))
+            {
+                return "decimal";
+            }
+            else if (type == typeof(double))
+            {
+                return "double";
+            }
+            else if (type == typeof(float))
+            {
+                return "float";
+            }
+            else if (type == typeof(int))
+            {
+                return "int";
             }
             else if (type == typeof(long))
             {
                 return "long";
             }
+            else if (type == typeof(object))
+            {
+                return "object";
+            }
+            else if (type == typeof(sbyte))
+            {
+                return "sbyte";
+            }
+            else if (type == typeof(short))
+            {
+                return "short";
+            }
+            else if (type == typeof(string))
+            {
+                return "string";
+            }
+            else if (type == typeof(uint))
+            {
+                return "uint";
+            }
             else if (type == typeof(ulong))
             {
                 return "ulong";
             }
-            else if (type == typeof(object))
+            else if (type == typeof(ushort))
             {
-                return "object";
+                return "ushort";
+            }
+            else if (type == typeof(void))
+            {
+                return "void";
             }
 
             var sb = new StringBuilder();
