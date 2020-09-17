2f155d23d7ce5f99ebba0049632b36462f8d452d
Pavel Djundik <xPaw@users.noreply.github.com>
2020-09-17 21:11:12 +0300

    Add default operating system conditionals

--- ValveKeyValue/ValveKeyValue/KVSerializerOptions.cs
+++ ValveKeyValue/ValveKeyValue/KVSerializerOptions.cs
@@ -1,20 +1,17 @@
-﻿namespace ValveKeyValue
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace ValveKeyValue
 {
     /// <summary>
     /// Options to use when deserializing a KeyValues file.
     /// </summary>
     public sealed class KVSerializerOptions
     {
-        string[] conditions;
-
         /// <summary>
         /// Gets or sets a list of conditions to use to match conditional values.
         /// </summary>
-        public string[] Conditions
-        {
-            get { return conditions ?? new string[0]; }
-            set { conditions = value; }
-        }
+        public IList<string> Conditions { get; set; } = new List<string>();
 
         /// <summary>
         /// Gets or sets a value indicating whether gets or sets if the parser should translate escape sequences (e.g. <c>\n</c>, <c>\t</c>).
@@ -26,6 +23,27 @@
         /// </summary>
         public IIncludedFileLoader FileLoader { get; set; }
 
+        public KVSerializerOptions()
+        {
+            // TODO: In the future we will want to skip this for consoles and mobile devices?
+            Conditions.Add("WIN32");
+            
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Conditions.Add("WINDOWS");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Conditions.Add("LINUX");
+                Conditions.Add("POSIX");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Conditions.Add("OSX");
+                Conditions.Add("POSIX");
+            }
+        }
+        
         /// <summary>
         /// Gets the default options (used when none are specified).
         /// </summary>
