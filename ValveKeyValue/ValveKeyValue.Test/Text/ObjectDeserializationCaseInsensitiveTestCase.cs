a4cef9a8aacdfb92491314176216bf11a4b4cfc0
Pavel Djundik <xPaw@users.noreply.github.com>
2020-11-14 12:11:32 +0200

    Add tests for key casing in deserialization

---
+++ ValveKeyValue/ValveKeyValue.Test/Text/ObjectDeserializationCaseInsensitiveTestCase.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace ValveKeyValue.Test
+{
+    class ObjectDeserializationCaseInsensitiveTestCase
+    {
+        [Test]
+        public void CanDeserializeObjectListAndPropertyName()
+        {
+            var text = TestDataHelper.ReadTextResource("Text.serialization_expected.vdf");
+            var actual = KVSerializer.Create(KVSerializationFormat.KeyValues1Text).Deserialize<DataObject[]>(text, KVSerializerOptions.DefaultOptions);
+            
+            Assert.AreEqual("Dota 2", actual[0].Name);
+            Assert.AreEqual("Dota 2 is a complex game where you get sworn at\nin Russian all the time.", actual[0].Summary);
+
+            Assert.AreEqual("Valve Software", actual[1].Developer);
+            Assert.AreEqual("Known as \"America's #1 war-themed hat simulator\", this game lets you wear stupid items while killing people.", actual[1].Summary);
+        }
+
+        [Test]
+        public void CanDeserializeFromRandomlyCasedKeys()
+        {
+            var text = TestDataHelper.ReadTextResource("Text.random_case_object.vdf");
+            var actual = KVSerializer.Create(KVSerializationFormat.KeyValues1Text).Deserialize<DataObject>(text, KVSerializerOptions.DefaultOptions);
+            
+            Assert.AreEqual("Dota 2", actual.Name);
+            Assert.AreEqual("Dota 2 is a complex game where you get sworn at\nin Russian all the time.", actual.Summary);
+            Assert.AreEqual("Valve Software", actual.Developer);
+        }
+
+        class DataObject
+        {
+            public string Name { get; set; }
+
+            public string Developer { get; set; }
+
+            [KVProperty("description")]
+            public string Summary { get; set; }
+
+            [KVIgnore]
+            public string ExtraData { get; set; }
+        }
+    }
+}
