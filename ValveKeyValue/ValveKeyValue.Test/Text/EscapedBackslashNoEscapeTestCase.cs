a2e0acd2947722717f5f2980554c050e653880c5
Pavel Djundik <xPaw@users.noreply.github.com>
2021-01-19 10:39:56 +0200

    Add a test for KV that has backwards slash at the end of a string

---
+++ ValveKeyValue/ValveKeyValue.Test/Text/EscapedBackslashNoEscapeTestCase.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace ValveKeyValue.Test
+{
+    class EscapedBackslashNoEscapeTestCase
+    {
+        [Test]
+        public void ParsesStringWithBackwardsSlashAtTheEnd()
+        {
+            Assert.That((string)data["BuildOutput"], Is.EqualTo(@"..\output\"));
+        }
+
+        KVObject data;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var options = new KVSerializerOptions { HasEscapeSequences = false };
+            using (var stream = TestDataHelper.OpenResource("Text.escaped_backslash_single_slash.vdf"))
+            {
+                data = KVSerializer.Create(KVSerializationFormat.KeyValues1Text).Deserialize(stream, options);
+            }
+        }
+    }
+}
