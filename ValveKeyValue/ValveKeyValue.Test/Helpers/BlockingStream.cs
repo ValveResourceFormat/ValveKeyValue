3c4ff2792c701e9e04060d91c6afe21f66c49172
Archi <JustArchi@JustArchi.net>
2022-01-14 15:34:07 +0100

    Add unit tests for #48
    The failing one is 1, the 4096 one verifies correctness of the implementation

---
+++ ValveKeyValue/ValveKeyValue.Test/Helpers/BlockingStream.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace ValveKeyValue.Test.Helpers;
+
+/// <summary>
+///     Example implementation of <see cref="Stream" /> that reads at most <see cref="MaxReadAtOnce" /> characters during a
+///     single <see cref="Read" /> operation, blocking in the process.
+/// </summary>
+internal sealed class BlockingStream : Stream
+{
+    private static readonly byte[] TestData = Encoding.UTF8.GetBytes(
+        @"""test_kv""
+		{
+			""test""	""1337""
+		}"
+    );
+
+    private long _backingPosition;
+
+    internal BlockingStream(int maxReadAtOnce = 1)
+    {
+        if (maxReadAtOnce <= 0) throw new ArgumentOutOfRangeException(nameof(maxReadAtOnce));
+
+        MaxReadAtOnce = maxReadAtOnce;
+    }
+
+    public int MaxReadAtOnce { get; }
+
+    public override bool CanRead => true;
+    public override bool CanSeek => false;
+    public override bool CanWrite => false;
+    public override long Length => TestData.Length;
+
+    public override long Position
+    {
+        get => _backingPosition;
+        set => throw new NotSupportedException();
+    }
+
+    public override void Flush()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        ArgumentNullException.ThrowIfNull(buffer);
+
+        if (offset < 0) throw new ArgumentOutOfRangeException(nameof(offset));
+
+        if (count <= 0) throw new ArgumentOutOfRangeException(nameof(count));
+
+        if (_backingPosition >= TestData.Length) return 0;
+
+        // Never read more than we have
+        if (count > TestData.Length - _backingPosition) count = TestData.Length - (int) _backingPosition;
+
+        // Never read more than user-configured limit
+        if (count > MaxReadAtOnce) count = MaxReadAtOnce;
+
+        for (var i = 0; i < count; i++) buffer[offset++] = TestData[_backingPosition++];
+
+        return count;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotSupportedException();
+    }
+}
--- ValveKeyValue/ValveKeyValue.Test/Text/SteamDataTestCase.cs
+++ ValveKeyValue/ValveKeyValue.Test/Text/SteamDataTestCase.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Globalization;
+using NUnit.Framework;
+using ValveKeyValue.Test.Helpers;
 
 namespace ValveKeyValue.Test
 {
@@ -24,6 +26,17 @@ namespace ValveKeyValue.Test
             Assert.That(
                 (string)data["config"][@"2\"],
                 Is.EqualTo(@" TF2_bak;del /q /s bin\*;cd tf;del /q /s *.cache media\*.mov;mkdir download;cd download;move ..\maps .\;move ..\materials .\;move ..\models .\;move ..\particles .\;move ..\resource .\;move ..\sound .\;move ..\scripts .\;cd resource;del game.ico tf.ttf tf2.ttf tf2build.ttf tf2professor.ttf tf2secondary.ttf tfd.ttf;cd ..\sound\ui;del gamestartup*.mp3 tv_tune*.mp3 holiday\gamestartup_*.mp3;:EOF"));
+        }
+        
+        [TestCase(1)]
+        [TestCase(4096)]
+        public void CanHandleBlockingStreams(int maxReadAtOnce)
+        {
+            using var blockingStream = new BlockingStream(maxReadAtOnce);
+
+            KVObject data = KVSerializer.Create(KVSerializationFormat.KeyValues1Text).Deserialize(blockingStream);
+            
+            Assert.That(data["test"].ToInt32(CultureInfo.InvariantCulture), Is.EqualTo(1337));
         }
     }
 }
