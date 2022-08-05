e53d3a3d97223c3dc2acce2642cf0df7f581cd6b
Pavel Djundik <xPaw@users.noreply.github.com>
2022-08-05 13:16:48 +0300

    Basic test for flagged value

--- ValveKeyValue/ValveKeyValue.Test/TextKV3/Basic.cs
+++ ValveKeyValue/ValveKeyValue.Test/TextKV3/Basic.cs
@@ -12,5 +12,14 @@ namespace ValveKeyValue.Test.TextKV3
 
             Assert.That((string)data["foo"], Is.EqualTo("bar"));
         }
+
+        [Test]
+        public void DeserializesFlaggedValues()
+        {
+            using var stream = TestDataHelper.OpenResource("TextKV3.flagged_value.kv3");
+            var data = KVSerializer.Create(KVSerializationFormat.KeyValues3Text).Deserialize(stream);
+
+            Assert.That((string)data["foo"], Is.EqualTo("bar"));
+        }
     }
 }
