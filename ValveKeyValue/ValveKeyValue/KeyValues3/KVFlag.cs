5cb87c2949cb9e5285c709e1549f61c42de85133
Pavel Djundik <xPaw@users.noreply.github.com>
2022-08-08 11:25:56 +0300

    Prepare kv flag handling

---
+++ ValveKeyValue/ValveKeyValue/KeyValues3/KVFlag.cs
@@ -0,0 +1,13 @@
+﻿namespace ValveKeyValue.KeyValues3
+{
+    public enum KVFlag
+    {
+        None = 0,
+        Resource = 1,
+        ResourceName = 2,
+        // TODO: We don't know what 4 was
+        Panorama = 8,
+        SoundEvent = 16,
+        SubClass = 32,
+    }
+}
---
+++ ValveKeyValue/ValveKeyValue/KeyValues3/KVFlaggedObjectValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ValveKeyValue.KeyValues3
+{
+    class KVFlaggedObjectValue<TObject> : KVObjectValue<TObject>
+        where TObject : IConvertible
+    {
+        public KVFlag Flag { get; private set; }
+
+        public KVFlaggedObjectValue(TObject value, KVFlag flag, KVValueType valueType)
+            : base(value, valueType)
+        {
+            Flag = flag;
+        }
+    }
+}
