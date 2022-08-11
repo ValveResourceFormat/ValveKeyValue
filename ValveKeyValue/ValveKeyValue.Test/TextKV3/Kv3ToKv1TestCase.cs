8fdeeba069f36c19e070b35f7fa95df641431a5c
Pavel Djundik <xPaw@users.noreply.github.com>
2022-08-11 11:59:54 +0300

    Test that we serialize to kv1 without flags and arrays

---
+++ ValveKeyValue/ValveKeyValue.Test/TextKV3/Kv3ToKv1TestCase.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace ValveKeyValue.Test.TextKV3
+{
+    class Kv3ToKv1TestCase
+    {
+        [Test]
+        public void SerializesAndDropsFlags()
+        {
+            using var stream = TestDataHelper.OpenResource("TextKV3.flagged_value.kv3");
+            var expected = TestDataHelper.ReadTextResource("TextKV3.flagged_value_kv1.vdf");
+
+            var kv1 = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
+            var kv3 = KVSerializer.Create(KVSerializationFormat.KeyValues3Text);
+            var data = kv3.Deserialize(stream);
+
+            data.Add(new KVObject("test", "success"));
+
+            string text;
+            using (var ms = new MemoryStream())
+            {
+                kv1.Serialize(ms, data);
+
+                ms.Seek(0, SeekOrigin.Begin);
+                using var reader = new StreamReader(ms);
+                text = reader.ReadToEnd();
+            }
+
+            Assert.That(text, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void SerializesArraysToObjects()
+        {
+            using var stream = TestDataHelper.OpenResource("TextKV3.array.kv3");
+            var expected = TestDataHelper.ReadTextResource("TextKV3.array_kv1.vdf");
+
+            var kv1 = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
+            var kv3 = KVSerializer.Create(KVSerializationFormat.KeyValues3Text);
+            var data = kv3.Deserialize(stream);
+
+            data.Add(new KVObject("test", "success"));
+
+            string text;
+            using (var ms = new MemoryStream())
+            {
+                kv1.Serialize(ms, data);
+
+                ms.Seek(0, SeekOrigin.Begin);
+                using var reader = new StreamReader(ms);
+                text = reader.ReadToEnd();
+            }
+
+            System.Console.WriteLine(text);
+
+            Assert.That(text, Is.EqualTo(expected));
+        }
+    }
+}
