2a9a054f5c812e77e42d18bde2d6447af7062125
Pavel Djundik <xPaw@users.noreply.github.com>
2023-11-10 23:06:28 +0200

    Add missing tests for kv object setter and `Add`

---
+++ ValveKeyValue/ValveKeyValue.Test/KVObjectTestCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ValveKeyValue.Test
+{
+    class KVObjectTestCase
+    {
+        [Test]
+        public void IndexerSetterWorks()
+        {
+            var obj = new KVObject("root", Array.Empty<KVObject>());
+
+            Assert.That(obj["this_is_set_in_test"], Is.Null);
+            obj["this_is_set_in_test"] = "some cool data";
+            Assert.That((string)obj["this_is_set_in_test"], Is.EqualTo("some cool data"));
+        }
+
+        [Test]
+        public void AddCallWorks()
+        {
+            var obj = new KVObject("root", Array.Empty<KVObject>());
+
+            Assert.That(obj["this_is_set_in_test"], Is.Null);
+            obj.Add(new KVObject("this_is_set_in_test", "some cool data"));
+            Assert.That((string)obj["this_is_set_in_test"], Is.EqualTo("some cool data"));
+        }
+    }
+}
