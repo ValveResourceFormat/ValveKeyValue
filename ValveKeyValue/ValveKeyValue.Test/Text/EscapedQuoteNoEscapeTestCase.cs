7d216dcca05445225b192ca6ef9d0158fcc0610b
Pavel Djundik <xPaw@users.noreply.github.com>
2024-01-14 22:04:41 +0200

    Add test case for escaped quote

---
+++ ValveKeyValue/ValveKeyValue.Test/Text/EscapedQuoteNoEscapeTestCase.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace ValveKeyValue.Test
+{
+    class EscapedQuoteNoEscapeTestCase
+    {
+        [Test]
+        public void ParsesStringWithBackwardsSlashWithQuoteAtTheEnd()
+        {
+            Assert.That((string)data["key"], Is.EqualTo("some value\\\""));
+            Assert.That((string)data["foo"], Is.EqualTo("bar"));
+        }
+
+        KVObject data;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var options = new KVSerializerOptions { HasEscapeSequences = false };
+            using var stream = TestDataHelper.OpenResource("Text.escaped_ending_quote.vdf");
+            data = KVSerializer.Create(KVSerializationFormat.KeyValues1Text).Deserialize(stream, options);
+        }
+    }
+}
