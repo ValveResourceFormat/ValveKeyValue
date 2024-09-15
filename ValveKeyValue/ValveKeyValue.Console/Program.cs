29a24e8ec79e6179dc6f8220b9d41f1b42d6414b
Pavel Djundik <xPaw@users.noreply.github.com>
2024-09-15 23:54:26 +0300

    Add cli args to console app

--- ValveKeyValue/ValveKeyValue.Console/Program.cs
+++ ValveKeyValue/ValveKeyValue.Console/Program.cs
@@ -1,34 +1,43 @@
 using System.Text;
 using ValveKeyValue;
+using ConsoleAppFramework;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-if (args.Length < 1)
+ConsoleApp.Run(args, Execute);
+
+/// <summary>
+/// Parse a KeyValues file and print to console.
+/// </summary>
+/// <param name="file">-f, Input file to be parsed.</param>
+/// <param name="escape">Whether the parser should translate escape sequences.</param>
+/// <param name="valve_null_bug">Whether invalid escape sequences should truncate strings rather than throwing.</param>
+static int Execute(
+    string file,
+    bool escape = false,
+    bool valve_null_bug = false
+)
 {
-    Console.Error.WriteLine("Provide path to a keyvalues file as the first argument.");
-    return 1;
+    if (!File.Exists(file))
+    {
+        Console.Error.WriteLine($"File \"{file}\" does not exist.");
+        return 1;
+    }
+
+    using var stream = File.OpenRead(file);
+
+    var options = new KVSerializerOptions
+    {
+        HasEscapeSequences = escape,
+        EnableValveNullByteBugBehavior = valve_null_bug 
+    };
+    var serializer = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
+    var root = serializer.Deserialize(stream, options);
+
+    RecursivePrint(root);
+
+    return 0;
 }
-
-var file = args[0];
-
-if (!File.Exists(file))
-{
-    Console.Error.WriteLine($"File \"{file}\" does not exist.");
-    return 1;
-}
-
-using var stream = File.OpenRead(file);
-
-var options = new KVSerializerOptions
-{
-    HasEscapeSequences = true,
-};
-var serializer = KVSerializer.Create(KVSerializationFormat.KeyValues1Text);
-var root = serializer.Deserialize(stream, options);
-
-RecursivePrint(root);
-
-return 0;
 
 static void RecursivePrint(KVObject obj, int indent = 0)
 {
