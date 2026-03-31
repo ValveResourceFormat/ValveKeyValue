ed013e8eb6dd2101ca1d429cbdd2f9eaed13587f
Pavel Djundik <xPaw@users.noreply.github.com>
2026-03-31 13:08:21 +0300

    Change KVToken to readonly record struct

--- ValveKeyValue/ValveKeyValue/KVToken.cs
+++ ValveKeyValue/ValveKeyValue/KVToken.cs
@@ -1,20 +1,4 @@
 namespace ValveKeyValue
 {
-    class KVToken
-    {
-        public KVToken(KVTokenType type)
-            : this(type, null)
-        {
-        }
-
-        public KVToken(KVTokenType type, string value)
-        {
-            TokenType = type;
-            Value = value;
-        }
-
-        public KVTokenType TokenType { get; }
-
-        public string Value { get; }
-    }
+    readonly record struct KVToken(KVTokenType TokenType, string Value = null);
 }
