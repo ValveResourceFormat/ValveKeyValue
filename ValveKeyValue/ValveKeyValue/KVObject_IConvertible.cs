27f501fc028cda19872c5d9f803450441f54c194
Pavel Djundik <xPaw@users.noreply.github.com>
2026-08-25 11:07:41 +0300

    Revert GetTypeCode visibility

--- ValveKeyValue/ValveKeyValue/KVObject_IConvertible.cs
+++ ValveKeyValue/ValveKeyValue/KVObject_IConvertible.cs
@@ -7,7 +7,7 @@ namespace ValveKeyValue
     {
         #region IConvertible (explicit interface)
 
-        public TypeCode GetTypeCode() => ValueType switch
+        TypeCode IConvertible.GetTypeCode() => ValueType switch
         {
             KVValueType.Boolean => TypeCode.Boolean,
             KVValueType.String => TypeCode.String,
